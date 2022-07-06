91079d2deb7e8ca43ceda5f64f5633df93d5610d
Manideep <kilarumanideep76@example.com>
2022-07-06 11:37:59 -0500

    fetch

--- client/src/components/pages/profilePage.js
+++ client/src/components/pages/profilePage.js
@@ -5,7 +5,19 @@ function Profile() {
     const currentuser = JSON.parse(localStorage.getItem('username'));
     console.log(currentuser);
 
+    const fetchArticles = () => {
+        fetchData(`/post/getpost`, { currentuser }, 'GET')
+            .then((articles) => {
+                console.log(articles)
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
 
+    useEffect(() => {
+        fetchArticles();
+    }, []);
 
     return (
         <div className="profile">
